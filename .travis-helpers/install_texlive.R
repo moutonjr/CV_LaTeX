8ef380f02c429b2cc69357cfe57d8620fae2f324
moutonjr <g@jserv.fr>
2020-06-08 18:11:27 +0200

    Squash merge integration of Travis-CI.

---
+++ .travis-helpers/install_texlive.R
@@ -0,0 +1,16 @@
+if (!requireNamespace("tinytex", quietly = TRUE)) {
+	if (!requireNamespace("devtools", quietly = TRUE)) {
+		install.packages("jsonlite", repos = "https://cran.rstudio.com/", quiet = TRUE)
+		install.packages("httr", repos = "https://cran.rstudio.com/", quiet = TRUE)
+		install.packages("memoise", repos = "https://cran.rstudio.com/", quiet = TRUE)
+		install.packages("devtools", repos = "https://cran.rstudio.com/", quiet = TRUE)
+		cat('devtools installed\n')
+	}
+	devtools::install_github(c('yihui/tinytex'), quiet = TRUE)
+	tinytex::install_tinytex()
+	tinytex:::install_yihui_pkgs()
+	tinytex::tlmgr_install('fontawesome', quiet = TRUE)
+	tinytex::tlmgr_install('psnfss', quiet = TRUE)
+	tinytex::tlmgr_install('graphics', quiet = TRUE)
+	tinytex::tlmgr_install('tools', quiet = TRUE)
+}
